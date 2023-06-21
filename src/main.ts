b733570af84889302243eb0e89018e1a939b4cd8
daviddiazh <daviddiazlds1221@gmail.com>
2023-06-20 21:06:08 -0500

    change in ports

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT);
+  const PORT = process.env.PORT || 8080;
 
-  console.log('🚀 Server running on port', +process.env.PORT);
+  await app.listen(PORT);
+
+  console.log('🚀 Server running on port', PORT);
 }
 bootstrap();
